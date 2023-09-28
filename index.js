bdc945dcc721800cdc1c011c3b9e407dcf9f006e
Alin <at.silcock@gmail.com>
2023-09-28 12:15:26 -0600

    continued functions lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activty = 'roller-skate'){
+    if (typeof saturdayFun === 'function') {
+        return `This Saturday, I want to ${activty}!`;
+    }
+}
+
+function mondayWork(activty = 'go to the office') {
+    if (typeof mondayWork === 'function') {
+        return `This Monday, I will ${activty}.`
+    }
+}
+
+function wrapAdjective(flair = "*"){
+    if (typeof wrapAdjective === 'function');
+    return function (wrapAdjective = "special") {
+        return `You are ${flair}${wrapAdjective}${flair}!`
+    }
+}
